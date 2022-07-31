3213bd3c97d32a09c3926c7ade2564078f0747fe
Lauren Williams <lauren@Lauren_Dell.localdomain>
2022-07-30 21:02:58 -0500

    basic get function added

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//router 
+	r := gin.Default()
+	r.GET("/", Get)
+	r.Run(":8090")
+}
+
+func Get(c *gin.Context) {
+	c.String(200,"Hello World!")
 }
