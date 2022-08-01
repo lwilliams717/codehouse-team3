29d3fb959a3837af8c03992bad9f9f90baf630b9
vgade <vgade@vmware.com>
2022-08-01 12:16:03 -0400

    http-request

--- my-app/src/app/landing/landing.component.ts
+++ my-app/src/app/landing/landing.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface TutorType {
+    id: string,
+    user_type: string,
+    name: string,
+    primary_subject: string,
+    secondary_subject: string,
+    description: string
+  }
+  
+  export interface TutorMatchedRecords {
+    matched_tutors: TutorType[]
+  }
+
 
 @Component({
     selector: 'app-landing',
@@ -7,9 +22,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
     show = true;
-    constructor() { }
+    matched_tutors!: TutorType[];
 
-    ngOnInit(): void {
-    }
+    constructor(private httpClient: HttpClient) {
+        this.httpClient = httpClient;
+      }
+
+  ngOnInit(): void {
+    this.httpClient.post<TutorMatchedRecords>('http://localhost:8090/api/tutors', {
+      primary_subject: '', secondary_subject: 'Civil'
+    }).subscribe(data => { 
+        this.matched_tutors = data.matched_tutors;
+     })
+  }
 
 }
