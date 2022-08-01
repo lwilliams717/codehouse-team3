0e382da07ed0e52941a508eae225a8371ab19b3e
Camila <cflor133@knights.ucf.edu>
2022-08-01 14:48:03 -0400

    added antonio :)

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingComponent } from './landing/landing.component';
+import { PeopleComponent } from './people/people.component';
 import { TestComponent } from './test/test.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,7 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     LandingComponent,
-    TestComponent
+    TestComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
--- my-app/src/app/people/people.component.ts
+++ my-app/src/app/people/people.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+import { HttpClient } from '@angular/common/http';
+
+export interface TutorType {
+    id: string,
+    user_type: string,
+    name: string,
+    primary_subject: string,
+    secondary_subject: string,
+    description: string
+  }
+  
+  export interface TutorMatchedRecords {
+    matched_tutors: TutorType[]
+  }
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './people.component.html',
-  styleUrls: ['./people.component.scss']
+    selector: 'app-people',
+    templateUrl: './people.component.html',
+    styleUrls: ['./people.component.scss']
 })
-export class PeopleComponent {
-  title = 'my-app';
-  show = true;
+export class PeopleComponent implements OnInit {
+    show = true;
+    tutors!: TutorType[];
 
-  addPeopleBlock() {
-    
+    constructor(private httpClient: HttpClient) {
+        this.httpClient = httpClient;
+      }
+
+  ngOnInit(): void {
+    this.httpClient.post<TutorMatchedRecords>('http://localhost:8090/api/tutors', {
+      primary_subject: '', secondary_subject: 'Physics'
+    }).subscribe(data => { 
+        this.tutors = data.matched_tutors;
+     })
   }
 
 }
-
