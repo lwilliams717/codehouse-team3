4de464e97fa40f496eb599130364eb56c1c9f1e9
adrimartinez174 <53013976+adrimartinez174@users.noreply.github.com>
2022-07-31 19:31:32 -0700

    adding landing page and testing

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+  show = true;
+}
+
+// //import {Component} from '@angular/core';
+
+// @Component({
+//     selector: 'ngb-toast', 
+//     templateUrl: './app.component.html'
+// })
+
+// export class NgbdToastInline {
+//   show = true;
+// }
---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './landing/landing.component';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
